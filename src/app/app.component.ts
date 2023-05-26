7c7e5a7d5f486de61edd43f6baa047f34e5bb009
Anmolsri07 <anmolsri001.dmk@gmail.com>
2023-05-26 18:00:13 +0530

    fix my bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { UserDataService } from './services/user-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Api';
+  users: any;
+  constructor(private userData: UserDataService) {
+    userData.Users().subscribe((data) => {
+      console.log(data);
+      this.users = data;
+    });
+    // console.log(this.users);
+  }
+  getuserFormData(info: any) {
+    // console.log(info);
+    this.userData.saveUser(info).subscribe((result) => {
+      console.log(result);
+    });
+  }
 }
